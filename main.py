dbb54ac820220fb263a821b8964e2b7e54fbec31
genius-gem <olatundetaiwo2109@gmail.com>
2026-09-01 16:23:57 +0100

    Initial Sayana Press Android WebView app

---
+++ main.py
@@ -0,0 +1,21 @@
+﻿
+import webview
+
+
+WEBSITE_URL = "https://sayanapress.com.ng"
+
+
+def main():
+    webview.settings["OPEN_EXTERNAL_LINKS_IN_BROWSER"] = True
+    webview.settings["ALLOW_DOWNLOADS"] = True
+
+    webview.create_window(
+        "Sayana Press",
+        WEBSITE_URL
+    )
+
+    webview.start()
+
+
+if __name__ == "__main__":
+    main()
